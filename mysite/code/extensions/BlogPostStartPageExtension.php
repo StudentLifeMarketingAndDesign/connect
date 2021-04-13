697e895a2f32ee5b42fe5b68cde792363cf928cd
Dustin Quam <quamsta@gmail.com>
2021-04-13 11:51:22 -0500

    updated field to be more descriptive

--- mysite/code/extensions/BlogPostStartPageExtension.php
+++ mysite/code/extensions/BlogPostStartPageExtension.php
@@ -17,7 +17,7 @@ class BlogPostStartPageExtension extends DataExtension {
 
 	public function updateCMSFields(FieldList $fields) {
 
-		$fields->addFieldToTab('Root.Main', TagField::create('StartPages', 'Department', StartPage::get())->setCanCreate(true), 'Content');
+		$fields->addFieldToTab('Root.Main', TagField::create('StartPages', 'Department', StartPage::get())->setCanCreate(true)->setDescription('The post will show up on the above department(s) start page'), 'Content');
 		//$fields->addFieldToTab('Root.PostOptions', CheckboxField::create('HideFromStaffNews', 'Hide this post from the general news feed')->setDescription('When checked, this post will only show up on the departmental start pages above'));
 
 	}
